9878eae0102b9487c34daddce5f7a09677b48307
katliang <katliang@outlook.com>
2019-12-20 14:10:40 -0800

    add uploaded image into canvas

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
@@ -10,17 +10,28 @@ function FileInput({ handleChange }) {
 
 function App() {
 	const [file, setFile] = useState('');
+	const canvasRef = useRef(null);
+	const imageRef = useRef(null);
 
 	const handleChange = e => {
 		setFile(URL.createObjectURL(e.target.files[0]));
 	}
 
+	const updateCanvas = file => {
+		if (!file) return;
+		const canvas = canvasRef.current;
+		const image = imageRef.current;
+		const context = canvas.getContext('2d');
+		context.drawImage(image, 0, 0);
+	}
+
   return (
   	<div>
     	<FileInput 
-    	handleChange={handleChange}
+    		handleChange={handleChange}
     	/>
-    	<img src={file} alt="" />
+    	<canvas ref={canvasRef} width="600" height="600" />
+    	<img ref={imageRef} src={file} alt="" className="hidden" onLoad={() => updateCanvas(file)}/>
   	</div>
   );
 }
