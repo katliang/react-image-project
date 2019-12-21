7cf8a231ee87cf260d53a760f7033c9f398eda43
katliang <katliang@outlook.com>
2019-12-21 15:41:51 -0800

    install styled components, add click event to canvas to get rgba of a pixel

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React, { useState, useRef } from 'react';
 import ReactDOM from 'react-dom';
+import styled from 'styled-components';
 import './index.css';
 
 function FileInput({ handleChange }) {
@@ -8,8 +9,15 @@ function FileInput({ handleChange }) {
   );
 }
 
+const ColoredDiv = styled.div`
+  background-color: ${props => props.rgba};
+  height:20px;
+  width:20px;
+`
+
 function App() {
   const [file, setFile] = useState('');
+  const [rgba, setRgba] = useState('');
   const canvasRef = useRef(null);
   const imageRef = useRef(null);
 
@@ -25,13 +33,26 @@ function App() {
     context.drawImage(image, 0, 0);
   }
 
+  const getColor = e => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext('2d');
+    const rect = canvas.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    const pixel = context.getImageData(x, y, 1, 1);
+    const data = pixel.data;
+    const rgba = `rgba(${data[0]}, ${data[1]}, ${data[2]}, ${(data[3]/255)})`;
+    setRgba(rgba);
+  }
+
   return (
     <div>
       <FileInput 
         handleChange={handleChange}
       />
-      <canvas ref={canvasRef} width="600" height="600" />
-      <img ref={imageRef} src={file} alt="" className="hidden" onLoad={() => updateCanvas(file)}/>
+      <canvas ref={canvasRef} width="600" height="600" onClick={(e) => getColor(e)} />
+      <img ref={imageRef} src={file} alt="" className="hidden" onLoad={() => updateCanvas(file)} />
+      <ColoredDiv rgba={rgba} />{rgba}
     </div>
   );
 }
